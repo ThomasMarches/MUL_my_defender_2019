6487e43f11bf047875c19649e4be0840be145b15
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2020-02-27 15:05:59 +0100

    [ADD] | Background menu

--- src/menu/main_menu.c
+++ src/menu/main_menu.c
@@ -13,9 +13,7 @@ game_object_t *create_main_menu_buttons(void)
     game_object_t *object = NULL;
 
     object = create_game_object(object, (char *)TITLE_PATH, \
-    (sfVector2f) {TITLE_X, TITLE_Y}, DECOR);
-    init_appearing_object(object);
-    object->update = &update_appearing_object;
+    (sfVector2f) {0, 0}, DECOR);
     object = create_text_button(object, (char *)PLAY_BUTTON_PATH, \
     (sfVector2f) {PLAY_BUTTON_X, PLAY_BUTTON_Y});
     object->callback = &play_button_function;
