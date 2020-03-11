a838920b4a1f432bda819996f48d48b0b7e16ddc
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2020-03-11 12:49:47 +0100

    [FIX] | Interface position change

--- src/tower/init_board.c
+++ src/tower/init_board.c
@@ -34,25 +34,8 @@ void draw_board(sfRenderWindow *window, game_object_t *object)
     sfRenderWindow_drawText(window, info->points->text, NULL);
 }
 
-board_t *create_board_text(board_t *info)
+board_t *config_board_text(board_t *info)
 {
-    char *tmp = NULL;
-    
-    info->wave = malloc(sizeof(score_t));
-    info->points = malloc(sizeof(score_t));
-    info->life = malloc(sizeof(score_t));
-    if (info == NULL || info->wave == NULL || info->points == NULL || info->life == NULL)
-        return (NULL);
-    info->wave->score = 1;
-    info->life->score = 10;
-    info->points->score = 0;
-    tmp = my_nbr_to_str(info->wave->score);
-    info->wave->score_text = my_strcat((char *)"Wave : ", tmp);
-    tmp = my_nbr_to_str(info->life->score);
-    info->life->score_text = my_strcat((char *)"Life : ", tmp);
-    tmp = my_nbr_to_str(info->points->score);
-    info->points->score_text = my_strcat((char *)"Points : ", tmp);
-    free(tmp);
     info->wave->text = init_text(info->wave->score_text, 1700, 840, (char *) FONT_PATH);
     info->life->text = init_text(info->life->score_text, 1125, 840, (char *) FONT_PATH);
     info->points->text = init_text(info->points->score_text, 1400, 840,(char *) FONT_PATH);
@@ -62,14 +45,37 @@ board_t *create_board_text(board_t *info)
     return (info);
 }
 
+board_t *create_board_text(board_t *info)
+{
+    char *tmp = NULL;
+    
+    info->wave = malloc(sizeof(score_t));
+    info->points = malloc(sizeof(score_t));
+    info->life = malloc(sizeof(score_t));
+    if (info == NULL || info->wave == NULL || info->points == NULL || info->life == NULL)
+        return (NULL);
+    info->wave->score = 0;
+    info->life->score = 10;
+    info->points->score = 0;
+    tmp = my_nbr_to_str(info->wave->score);
+    info->wave->score_text = my_strcat((char *)"Wave : ", tmp);
+    tmp = my_nbr_to_str(info->life->score);
+    info->life->score_text = my_strcat((char *)"Life : ", tmp);
+    tmp = my_nbr_to_str(info->points->score);
+    info->points->score_text = my_strcat((char *)"Points : ", tmp);
+    free(tmp);
+    return (info);
+}
+
 game_object_t *create_board(game_object_t *last)
 {
     game_object_t *board = create_game_object(last, (char *) TOWER_BOARD_PATH, (sfVector2f) {0, 760}, TOWER_BOARD);
     board_t *info = malloc(sizeof(board_t));
 
-    if (board == NULL || info == NULL)
+    if (board == NULL || info == NULL)
         return (last);
     info = create_board_text(info);
+    info = config_board_text(info);
     board->z_index = 5;
     board->draw = &draw_board;
     board->extend = (void *) info;
