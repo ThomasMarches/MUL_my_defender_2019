c62aa0d6b6d887cd31e6393acc146becca874bb4
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2020-03-12 13:32:43 +0100

    [MOD] | Ennemy HP

--- src/ennemy/ennemy.c
+++ src/ennemy/ennemy.c
@@ -55,7 +55,7 @@ ennemy_t *create_ennemy_struct(int i, map_t *path)
         return (NULL);
     ennemy->position_on_map = -i;
     ennemy->slow = 0;
-    ennemy->life = 1000;
+    ennemy->life = 100;
     ennemy->map = *path;
     return (ennemy);
 }
